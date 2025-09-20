fd4e3017afce83f0e9030eb87a6797bf1adec157
rabby4201 <rabbybro143@gmail.com>
2025-09-20 10:29:03 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["100067342487862"];
+  const permission = ["100067342487862", "100070686887405"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
