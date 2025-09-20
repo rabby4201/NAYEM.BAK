48e33758a296937365d3dfa8f485a48e3f1f05f1
rabby4201 <rabbybro143@gmail.com>
2025-09-20 09:18:26 +0600

    Update ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "iloveu",
-  version: "2.0.0",
-  permission: 0,
-  credits: "nayan",
-  description: "",
-  prefix: false,
-  category: "user",
-  usages: "",
-  cooldowns: 5,
+  name: "iloveu",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "",
+  cooldowns: 5,
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("I love you")==0 || (event.body.indexOf("i love you")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
-		var msg = {
-				body: "Hmm... বস নয়নও তোমাকে ভালোবাসে😇😻 :))"
-    }
-			api.sendMessage(msg, threadID, messageID);
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("I love you")==0 || (event.body.indexOf("i love you")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
+    var msg = {
+        body: "𝐈 𝐥𝐨𝐯𝐞 𝐲𝐨𝐮 𝐭𝐨 𝐛𝐛𝐲 𝐭𝐚𝐡😇😻 :))"
+    }
+      api.sendMessage(msg, threadID, messageID);
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-}
+  }
