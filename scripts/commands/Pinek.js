63eb5ec179c9289f939e83a38b46cff6984ae3f1
rabby4201 <rabbybro143@gmail.com>
2025-10-03 09:50:54 +0600

    Create Pinek.js

---
+++ scripts/commands/Pinek.js
@@ -0,0 +1,45 @@
+module.exports.config = {
+    name: "bossadmin",
+    version: "1.0.0",
+    permission: 0,
+    credits: "nayan",
+    prefix: true,
+    description: "search results on google",
+    category: "without prefix",
+    usages: "google [text]",
+    cooldowns: 5,
+    dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+  hours = Math.floor(time / (60 * 60)),
+  minutes = Math.floor((time % (60 * 60)) / 60),
+  seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+  
+var callback = () => api.sendMessage({body:`
+--------------------------------------------
+--------------------------------------------
+𝐍𝐚𝐦𝐞       : 𝐌𝐝 𝐍𝐚𝐲𝐞𝐦 𝐊𝐡𝐚𝐧𝐝𝐨𝐤𝐞𝐫
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
+ 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐏𝐓𝐊.
+𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𝐖𝐨𝐫𝐤        : বাংলাদেশ রেলওয়ে (মাস্টার)🌺
+😐🤦‍♂️
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐈𝐧𝐛𝐨𝐱
+😝🙈
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/NAYEM.VAW.OKY`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI(`https://i.postimg.cc/cJcvkF4j/inbound6681128788107298143.jpg`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+   };
